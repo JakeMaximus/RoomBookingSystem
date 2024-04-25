20e71669074bdf7b1f7cf6290c21d470d2e4e9d2
Mannoj <84440007+JakeMaximus@users.noreply.github.com>
2024-04-25 18:53:14 +0800

    Add files via upload

---
+++ src/java_assignment/javaassignment.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package java_assignment;
+
+/**
+ *
+ * @author Mannoj
+ */
+public class javaassignment {
+    public static void main(String[] args) {
+        loginoriginal lg = new loginoriginal();
+        lg.setVisible(true);
+    }
+}
